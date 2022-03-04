d1175b1e83828c305a608627554376323fa16b23
arnab64g <79049651+arnab64g@users.noreply.github.com>
2022-03-04 20:58:37 +0600

    Add files via upload

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    public static void Main(String[] args){
+        string s = Console.ReadLine();
+        char[] ch = s.ToCharArray();
+        int len = s.Length;
+        bool[] isVisited = new bool[26];
+        for (int i = 0; i < len; i++){
+            if (!isVisited[ch[i] - 'a']){
+                Console.Write(ch[i]);
+                isVisited[ch[i] - 'a'] = true;
+            }
+        }
+        Console.WriteLine();
+    }
+}
